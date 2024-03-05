59e9c3b28b2eb02a8c08e55eaebf0176e5b4ec84
Douglas Gierow <douglas.gierow@slalom.com>
2024-03-05 12:38:09 -0700

    create landing component and add to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'tailwindcss/tailwind.css';
 import App from 'components/App';
 
 const container = document.getElementById('root') as HTMLDivElement;
