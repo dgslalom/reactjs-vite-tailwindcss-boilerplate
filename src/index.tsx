4a4451d0b9108985fd0b372e101a6848c85c2f96
Douglas Gierow <douglas.gierow@slalom.com>
2024-03-05 10:52:19 -0700

    reorg index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
 
 const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+const root = ReactDOM.createRoot(container)
 
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
